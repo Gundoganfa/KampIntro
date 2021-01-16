0e5c3ccc2ff6607fbe48528b5b5c13e5616bee59
Fatih Gündoğan <gundoganfa@gmail.com>
2021-01-16 23:53:23 +0300

    Deneme commiti

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -13,10 +13,12 @@ namespace DegerVeReferansTipler
 
             int[] sayilar1 = { 10, 20, 30 };
             int[] sayilar2 = { 100, 200, 300 };
+
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
 
             Console.WriteLine(sayilar1[0]);
+
         }
     }
 }
