4f24aad5b37a5e8274d297e2ecbcb3d852cdbee1
Fatih Gündoğan <gundoganfa@gmail.com>
2021-01-24 20:50:31 +0300

    Generic Dictionary

---
+++ GenericDictionary/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDictionary
+{
+
+    class MyDictionary <TKey, TValue>
+    {
+        TKey[] keyList;
+        TValue[] valueList;
+        TKey[] tempKeyList;
+        TValue[] tempValueList;
+
+        public MyDictionary()
+        {
+            // create instances for key and value lists
+            // just after a new instance of the class is created
+            keyList = new TKey[] { };
+            valueList = new TValue[] { };
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempKeyList = keyList;
+            tempValueList = valueList;
+
+            keyList = new TKey[keyList.Length + 1];
+            valueList = new TValue[valueList.Length + 1];
+
+            for (int i = 0; i < tempKeyList.Length; i++)
+            { //keyList and valueList have same length
+                keyList[i] = tempKeyList[i];
+                valueList[i] = tempValueList[i];
+            }
+            
+            keyList[keyList.Length - 1] = key;
+            valueList[keyList.Length - 1] = value;
+
+            Console.WriteLine(key + " " + value + " added!");
+        }
+    }
+}
---
+++ GenericDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("Father", "Bora");
+            dictionary.Add("Son1", "Alp");
+            dictionary.Add("Son2", "Tuncay");
+            dictionary.Add("Daughter1", "Jenny");
+
+        }
+    }
+}
