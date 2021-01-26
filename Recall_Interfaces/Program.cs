0bbd8dc2a3bfcca0e13253a1cce830265e89d1e6
Fatih Gündoğan <gundoganfa@gmail.com>
2021-01-26 22:55:23 +0300

    recall interface added

---
+++ Recall_Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recall_Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+       
+    }
+    class Student : IPerson
+    {
+    }
+}
